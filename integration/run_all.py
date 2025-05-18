e8dc26c2ce924fffa4caa831d414d246d52d2338
Beyza <141168063+Beyzajjk@users.noreply.github.com>
2025-05-18 23:39:36 +0300

    run_all.py

---
+++ integration/run_all.py
@@ -0,0 +1,16 @@
+# run_all.py
+
+def main():
+    print("Segmentasyon modülü başlatılıyor...")
+    # import segmentation.segment gibi yerleri sonra ekleyeceksin
+
+    print("Görsel üretim başlatılıyor...")
+    # import generation.generate_image
+
+    print("Metin anlatımı oluşturuluyor...")
+    # import narration.text_narration
+
+    print("Tüm modüller başarıyla çalıştırıldı.")
+
+if __name__ == "__main__":
+    main()
